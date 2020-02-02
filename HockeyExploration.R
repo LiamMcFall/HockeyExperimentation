3417d697adc9deeebe00ab502778f232cfc16690
Liam McFall <limcfall7@gmail.com>
2020-02-01 22:49:18 -0500

    Commit from Rstudio

--- HockeyExploration.R
+++ HockeyExploration.R
@@ -1,4 +1,3 @@
-library(haven)
 library(tidyverse)
 library(lubridate)
 library(magrittr)
@@ -7,7 +6,12 @@ events <- read.csv("/Users/liammcfall/HockeyExperimentation/nhl_events.csv")
 
 # Event cleaning
 
-events <- as.data.frame(events)
-events <- subset(events, select = -VAR1)
-events[events == "NA"] <- NA
+events <- subset(events, select = - c(X, VAR1))
+events$season <- factor(events$season)
+events$game_id <- factor(events$game_id)
+events$event_description <- as.character(events$event_description)
+events$event_detail <- as.character(events$event_detail)
+events$players_on <- as.character(events$players_on)
+events$players_off <- as.character(events$players_off)
 events$game_date <- date(events$game_date)
+
