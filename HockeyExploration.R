d16269db84efc4cba4355be8ca3b4cac926e2ecf
Liam McFall <limcfall7@gmail.com>
2020-02-01 23:44:34 -0500

    Commit from Rstudio

--- HockeyExploration.R
+++ HockeyExploration.R
@@ -31,5 +31,14 @@ unblocked_shot_types <- c('GOAL', 'MISS', 'SHOT')
 unblocked_shots <- events %>%
   filter(event_type %in% unblocked_shot_types)
 
-write.csv(unblocked_shots, "/Users/liammcfall/HockeyExperimentation/unblocked_shots.csv")
+#write.csv(unblocked_shots, "/Users/liammcfall/HockeyExperimentation/unblocked_shots.csv")
 
+unblocked_shots <- unblocked_shots %>%
+  mutate(shot_distance = sqrt((89 - abs(coords_x))^2 + coords_y^2),
+         shot_angle = abs(atan(coords_y / (89 - abs(coords_x))) * (180 / pi)),
+         is_goal = ifelse(event_type == 'GOAL', 1, 0))
+
+# Unblocked exploratory
+
+test_model <- glm(is_goal ~ shot_distance + shot_angle, data = unblocked_shots)
+summary(test_model)
